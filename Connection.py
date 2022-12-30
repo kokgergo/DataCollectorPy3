414001e149e0cc95396efd7702bb08f458f17ce9
kokgergo <kok.gergo@gmail.com>
2022-12-30 18:40:36 +0100

    Try to connect device

--- Connection.py
+++ Connection.py
@@ -1,25 +1,49 @@
 import asyncio
+import bleak
 from bleak import BleakClient
 from bleak import BleakScanner
 
+addresses = ["A4:C1:38:B1:5B:82", "A4:C1:38:F8:4A:20", "A4:C1:38:83:EA:AA", "A4:C1:38:CD:A5:B7"]
+address = "A4:C1:38:B1:5B:82" 
+
+async def checkDeviceAvaible(address):
+    found = await bleak.BleakScanner.find_device_by_address(device_identifier=address, timeout=10)
+    if found is None:
+        print("nothing")
+    else:
+        print(found)
+        await connect(found)
+
 async def discoverDevices():
-    devices = await BleakScanner.discover()
+    devices = await BleakScanner.discover(timeout=10)
     for d in devices:
         print(d)
 
+def main():
+    for addr in addresses:
+        print(addr)
+        asyncio.run(checkDeviceAvaible(addr))
 
 
-addresses = ["A4:C1:38:B1:5B:82" "A4:C1:38:F8:4A:20" "A4:C1:38:83:EA:AA" "A4:C1:38:CD:A5:B7"]
-address = "A4:C1:38:F8:4A:20" 
+
+
 MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
 ACTUAL_HUMIDITY_AND_TEMPERATURE = "ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6"
 BROADCAST_HUMIDITY_AND_TEMPERATURE = "00002902-0000-1000-8000-00805f9b34fb"
 
 async def connect(address):
     async with BleakClient(address) as client:
+        try:
+            await client.connect()
+        except TimeoutError: 
+            print("Timeout at connect to %s", address)
+            return
+        except :
+            print("Connection failed")
+            return
+            
         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
         print("Model Number: {0}".format("".join(map(chr, model_number))))
 
 
-
-asyncio.run(connect(address))
+main()
