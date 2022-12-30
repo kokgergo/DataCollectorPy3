712b7e1074716d91163e89b01a44cb9c60accc53
kokgergo <kok.gergo@gmail.com>
2022-12-30 15:54:02 +0100

    gi

--- Connection.py
+++ Connection.py
@@ -8,3 +8,11 @@ async def main():
         print(d)
 
 asyncio.run(main())
+
+address = "24:71:89:cc:09:05"
+MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
+
+async def connect():
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
