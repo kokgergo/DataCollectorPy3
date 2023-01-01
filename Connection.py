61123d9f285b3710e13545b6213389546150e428
kokgergo <kok.gergo@gmail.com>
2023-01-01 14:05:42 +0100

    Parser added

--- Connection.py
+++ Connection.py
@@ -32,18 +32,23 @@ ACTUAL_HUMIDITY_AND_TEMPERATURE = "ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6"
 BROADCAST_HUMIDITY_AND_TEMPERATURE = "00002902-0000-1000-8000-00805f9b34fb"
 
 async def connect(address):
-    async with BleakClient(address) as client:
-        """ try:
-            await client.connect()
-        except TimeoutError: 
-            print("Timeout at connect to %s", address)
-            return
-        except :
-            print("Connection failed")
-            return """
-            
+    async with BleakClient(address) as client:   
         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
         print("Model Number: {0}".format("".join(map(chr, model_number))))
+        actual_hum_and_temp = await client.read_gatt_char(ACTUAL_HUMIDITY_AND_TEMPERATURE)
+        temp_humid_parser(actual_hum_and_temp)
+
+def temp_humid_parser(data : bytearray):
+    """ First two byte is the temperature*100 data little endian
+        Third is humidity in %
+        Fourth and Fifth is Voltage level of the battery in mV little endian also
+    """
+    temp = data[0:1:-1]
+    print("Temperature: {0}", temp)
+    humidity = int(data[2])
+    print("Humidity: {0}", humidity)
+    battery = data[3:4:-1]
+    print("Battery: {0}", battery)
 
 
 main()
